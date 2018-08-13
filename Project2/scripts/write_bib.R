e385fc0c77e7d8d8af7b0dd50029f295391ae3c1
scienception <cate.cevallos@gmail.com>
2018-08-13 23:24:42 +0200

    both projects

---
+++ Project2/scripts/write_bib.R
@@ -0,0 +1,6 @@
+# Add the DT and rorcid packages to the function so the packages.bib file has the references we need for the embedded data table ORCID integration processes executed in this lesson.
+
+library(tidyverse)
+library(knitr)
+write_bib(c("tidyverse", "rmarkdown", "knitr", "DT", "rorcid"),
+          file = "packages.bib")
